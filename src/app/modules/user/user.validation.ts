c7ec47ac3f8078731a5ced6f8e3314d20aff166d
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-25 14:03:10 +0600

    Create user validation using zod.

---
+++ src/app/modules/user/user.validation.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+import { Gender } from "./user.constant";
+
+const createUserValidationSchema = z.object({
+  fullName: z.string({
+    invalid_type_error: "Full name must be string",
+    required_error: "Full name is required",
+  }),
+  userName: z.string({
+    invalid_type_error: "userName must be string",
+    required_error: "userName is required",
+  }),
+  email: z.string({
+    invalid_type_error: "Email must be string",
+    required_error: "Email is required",
+  }),
+  password: z.string({
+    invalid_type_error: "Password must be string",
+    required_error: "Password is required",
+  }),
+  age: z.number({
+    invalid_type_error: "Age must be number",
+    required_error: "Age is required",
+  }),
+  phone: z.string({
+    invalid_type_error: "Phone number must be string",
+    required_error: "Phone number is required",
+  }),
+  gender: z.enum(Gender as [string, ...string[]]),
+  profileImage: z
+    .string({
+      invalid_type_error: "profile image must be string",
+    })
+    .optional(),
+});
+
+export const UserValidations = {
+  createUserValidationSchema,
+};
