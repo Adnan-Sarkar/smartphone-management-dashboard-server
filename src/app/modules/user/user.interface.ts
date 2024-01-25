0848e5ff9ff7e131d4b152cf7f3aea9e69127d55
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-25 14:01:35 +0600

    Create user interface.

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  fullName: string;
+  userName: string;
+  email: string;
+  password: string;
+  phone: string;
+  gender: "male" | "female";
+  age: number;
+  profileImage?: string;
+}
