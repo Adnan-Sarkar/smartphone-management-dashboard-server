9917bfb891c0a3591c1f997250e4401478f6d009
Adnan-Sarkar <adnansarkar111@gmail.com>
2024-01-26 21:05:25 +0600

    Export more config from env file.

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -8,4 +8,7 @@ dotenv.config({
 export default {
   port: process.env.PORT,
   db_url: process.env.MONGODB_URL,
+  salt_rounds: process.env.SALT_ROUND,
+  jwt_access_token_secret: process.env.JWT_ACCESS_TOKEN_SECRET,
+  jwt_access_token_expires_in: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN,
 };
